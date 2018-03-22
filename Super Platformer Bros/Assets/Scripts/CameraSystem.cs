7d6d9f20dc51c222bb77b9e80757d601a32f2bfd
RiCK BROWN <ricky.brown.00@gmail.com>
2018-03-22 15:09:24 +0000

    Inital Commit

---
+++ Super Platformer Bros/Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    public GameObject Player;
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void LateUpdate()
+    {
+        float x = Mathf.Clamp(Player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(Player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+
+
+}
+
---
+++ Super Platformer Bros/Assets/Scripts/Player_Move_Prot.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move_Prot : MonoBehaviour {
+
+    private float moveX;
+    public int playerspeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1750;
+    public bool isGrounded;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded == true){
+            Jump();
+        }
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.1f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerspeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //JUMPING CODE
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("PLAYER HAS COLLIDED WITH " + collision.collider.name);
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
